16a59a1f375cc37f0a59266a46e02aa639fe1186
maldron0309 <bagjinhyeong640@gmail.com>
2023-11-21 00:09:56 +0900

    [add] monster animation, AI

--- Assets/CS/EnemyMovement.cs
+++ Assets/CS/EnemyMovement.cs
@@ -6,6 +6,7 @@ public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Animator anim;
+    private SpriteRenderer sr;
 
     // 행동 지표 결정할 변수
     public int nextMove;
@@ -14,6 +15,7 @@ public class EnemyMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
         
         Think();
         
@@ -27,23 +29,45 @@ public class EnemyMovement : MonoBehaviour
         rb.velocity = new Vector2(nextMove, rb.velocity.y);
         
         // Platform Check
-        Vector2 front = new Vector2(rb.position.x + nextMove, rb.position.y);
+        Vector2 front = new Vector2(rb.position.x + nextMove * 0.3f, rb.position.y);
         
         Debug.DrawRay(front,Vector3.down,new Color(0,1,0));
-        RaycastHit2D rayHit = Physics2D.Raycast(front, Vector2.down, 1,
+        RaycastHit2D rayHit = Physics2D.Raycast(front, Vector3.down, 1,
             LayerMask.GetMask("Platform"));
 
         if (rayHit.collider == null)
         {
-            Debug.Log("경고");
+            Turn();
         }
     }
 
     // 재귀 함수 : 자신을 스스로 호출하는 함수
     private void Think()
     {
+        // Set next Active
         nextMove = Random.Range(-1, 2);
         
-        Invoke("Think",5);
+        // sprite animation
+        anim.SetInteger("RunSpeed",nextMove);
+        
+        // Flip sprite
+        if (nextMove != 0)
+        {
+            sr.flipX = nextMove == 1;   
+        }
+        
+        // Recursive0
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think",nextThinkTime);
+    }
+
+    private void Turn()
+    {
+        nextMove *= -1;
+        sr.flipX = nextMove == 1;
+        
+        // CancelInvoke() : 현재 작동 중인 모든 Invoke함수를 멈추는 함수
+        CancelInvoke();
+        Invoke("Think",2);
     }
 }
